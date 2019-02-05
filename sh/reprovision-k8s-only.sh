a74badabb4e4938972a1dbda636dba70cffbdf26
Sorin Buliarca <sorin.buliarca@ft.com>
2019-02-05 14:21:31 +0200

    Added script to recreate k8s only cluster

--- sh/reprovision-k8s-only.sh
+++ sh/reprovision-k8s-only.sh
@@ -7,8 +7,11 @@ cd /ansible
 ansible-playbook --vault-password-file=vault.pass reprovision-k8s-only.yaml --extra-vars "\
 aws_region=${AWS_REGION} \
 cluster_name=${CLUSTER_NAME} \
+aws_access_key=${AWS_ACCESS_KEY} \
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY} \
 platform=${PLATFORM} \
 environment_type=${ENVIRONMENT_TYPE} \
 reuse_credentials=${REUSE_CREDENTIALS} \
 share_cluster_credentials=${SHARE_CLUSTER_CREDENTIALS} \
-cluster_environment=${CLUSTER_ENVIRONMENT} "
+cluster_environment=${CLUSTER_ENVIRONMENT} \
+oidc_issuer_url=${OIDC_ISSUER_URL}"
